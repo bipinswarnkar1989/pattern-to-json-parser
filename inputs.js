40810613658e1a6a53883ecf99cc0fb494134137
Bipin Swarnkar <bipin.swarnkar@Bipins-MacBook-Pro.local>
2019-10-15 22:53:32 +0530

    added parser function file

---
+++ inputs.js
@@ -0,0 +1,16 @@
+const input1 = `interview_attendance:P,intervie
+w_date:2019-04-15--to--2019-04-1
+5,status:CAP`;
+
+const input2 = `status:all,applied_date:2019-04
+-15--to--2019-04-15,screen_statu
+s:SR|NS`;
+
+const input3 = `location:mumbai|delhi|pune,list
+_type:S,min_education:1`;
+
+export {
+    input1,
+    input2,
+    input3
+}
---
+++ parser.js
@@ -0,0 +1,13 @@
+import { input1, input2, input3 } from './inputs';
+
+const parser = pattern => {
+    var result;
+     
+    return result;
+}
+
+const output1 = parser(input1);
+
+const output2 = parser(input2);
+
+const output3 = parser(input3);
